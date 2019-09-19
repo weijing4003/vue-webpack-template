1f4bf661b92bae9af9bd607d802e8fd3109f3836
weijing <812065568@qq.com>
2019-09-19 21:47:55 +0800

    Initial commit

---
+++ build/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+module.exports = {
+    //指定的打包形式
+    mode: 'development',
+    entry: {
+        //配置文件的入口
+        main: path.resolve(__dirname, '../src/main.js')
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'js/[name].[hash:8].js',
+        chunkFilename: 'js/[name].[hash:8].js',
+        publicPath: '/'
+
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+                ]
+            }
+        ]
+    }
+}
