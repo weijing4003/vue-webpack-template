c1c439d335a3d7cfc57cc54e0a3a9aa12ab06c94
weijing <812065568@qq.com>
2019-09-21 11:53:00 +0800

    [webpack]

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const VuePloaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
     //指定的打包形式
@@ -7,6 +9,16 @@ module.exports = {
     entry: {
         //配置文件的入口  babel-polyfill 对一些不支持新语法的客户端提供新语法的实现
         main: ["@babel/polyfill", path.resolve(__dirname, '../src/main.js')]
+    },
+    devServer: {
+        hot: true,
+        port: 3000,
+        contentBase: './dist'
+    },
+    resolve: {
+        alias: {
+            vue$: 'vue/dist/vue.runtime.esm.js'
+        }
     },
     output: {
         path: path.resolve(__dirname, '../dist'),
@@ -41,12 +53,96 @@ module.exports = {
                         loader: 'postcss-loader'  //配置 postcss 实现自动添加css3前缀
                     }
                 ]
+            },
+            //打包图片 媒体 字体等文件
+            {
+                test: /\.(jpe?g|png|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 4096,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    name: 'img/[name].[hash:8].[ext]'
+                                }
+                            }
+                        }
+                    },
+                ]
+
+            },{
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|acc)(\?.*)?$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 4096,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    name: 'media/[name].[hash:8].[ext]'
+                                }
+                            }
+                        }
+                    },
+                ]
+
+            },{
+                test: /\.(woff2|eot|ttf|otf)(\?.*)?$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 4096,
+                            fallback: {
+                                loader: 'file-loader',
+                                options: {
+                                    name: 'fonts/[name].[hash:8].[ext]'
+                                }
+                            }
+                        }
+                    },
+                ]
+
+            },{
+                test: /\.vue$/,
+                use: [
+                    {
+                        loader: 'cache-loader'
+                    },{
+                        loader: 'thead-loader'
+                    },{
+                        loader: 'vue-loader',
+                        options: {
+                            compilerOptions: {
+                                preserveWhitespace: false
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.jsx?$/,
+                use: [
+                    {
+                        loader: 'cache-loader'
+                    },{
+                        loader: 'thread-loader'
+                    },{
+                        loader: 'babel-loader'
+                    }
+                ]
             }
         ]
     },
-    plugins: {
+    plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, '../public/index.html')
-        })
-    }
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new VuePloaderPlugin()
+    ]
 }
--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log("hello webpack")
+
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
