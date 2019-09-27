669995fc4da06eec8337db5e3b5e17ed12bf45f1
weijing <812065568@qq.com>
2019-09-27 22:52:04 +0800

    router

---
+++ src/views/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h2>Home</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
