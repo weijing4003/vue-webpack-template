22bae715b35343d1dba28b4e6191bdff105c0602
weijing <812065568@qq.com>
2019-09-30 22:57:23 +0800

    file

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -153,11 +153,11 @@ module.exports = {
         }),
         new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        // new VueLoaderPlugin(),
-        // new webpack.DefinePlugin({
-        //     'process.env': {
-        //       VUE_APP_BASE_URL: JSON.stringify('http://localhost:3000')
-        //     }
-        //   }),
+        new VueLoaderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+              VUE_APP_BASE_URL: JSON.stringify('http://localhost:3000')
+            }
+          }),
     ]
 }
